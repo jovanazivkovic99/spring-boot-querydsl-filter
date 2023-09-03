b4b073faf599ce2dcb2a44fbe037e975ca88e84a
jovanazivkovic99 <jovanakipar@gmail.com>
2023-09-04 00:42:57 +0200

    finished everythinh

--- src/main/java/com/example/filteringquerydsl/controller/GameController.java
+++ src/main/java/com/example/filteringquerydsl/controller/GameController.java
@@ -2,11 +2,13 @@ package com.example.filteringquerydsl.controller;
 
 import com.example.filteringquerydsl.response.GameFilterResponse;
 import com.example.filteringquerydsl.service.GameService;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,22 +26,22 @@ public class GameController {
     @GetMapping("/filter")
     public Page<GameFilterResponse> findByOpponentUsername (
             @RequestParam(required = false) List<String> opponentUsernames,
-            @RequestParam(required = false) LocalDateTime dateFrom,
+            @RequestParam(required = false)@DateTimeFormat(pattern = "yyy-mm-dd") LocalDateTime dateFrom,
             @RequestParam(required = false) LocalDateTime dateTo,
             @RequestParam(required = false) String gameStatus,
-            @RequestParam(required = false) String winnerUsername,
+            @RequestParam(required = false) List<String> winnerUsernames,
             @RequestParam(required = false, defaultValue = "0") int pageNumber,
             @RequestParam(required = false, defaultValue = "10") int pageSize,
-            @RequestParam(defaultValue = "ALL") String sortDirection
+            @RequestParam(required = false, defaultValue = "date") String sortField,  // Default to "date"
+            @RequestParam(required = false, defaultValue = "desc") String sortDirection  // Default to "asc"
     ) {
-        Sort sort = Sort.unsorted(); // Default to unsorted
-        if ("ASC".equalsIgnoreCase(sortDirection)) {
-            sort = Sort.by(Sort.Order.asc("date"));
-        } else if ("DESC".equalsIgnoreCase(sortDirection)) {
-            sort = Sort.by(Sort.Order.desc("date"));
-        }
+        // Parse the sort direction parameter
+        Sort.Direction direction = Sort.Direction.fromString(sortDirection);
+
+        // Create a Sort object based on sortField and sortDirection
+        Sort sort = Sort.by(direction, sortField);
         Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
         return gameService.filterGames(
-                opponentUsernames, dateFrom, dateTo, gameStatus, winnerUsername, pageable);
+                opponentUsernames, dateFrom, dateTo, gameStatus, winnerUsernames, pageable);
     }
 }
--- src/main/java/com/example/filteringquerydsl/service/GameService.java
+++ src/main/java/com/example/filteringquerydsl/service/GameService.java
@@ -2,16 +2,13 @@ package com.example.filteringquerydsl.service;
 
 import com.example.filteringquerydsl.mapper.GameMapper;
 import com.example.filteringquerydsl.model.Game;
-import com.example.filteringquerydsl.model.QGame;
 import com.example.filteringquerydsl.model.QGamePlayer;
 import com.example.filteringquerydsl.model.QPlayer;
 import com.example.filteringquerydsl.response.GameFilterResponse;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.core.types.dsl.SimplePath;
-import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -19,88 +16,127 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.query.QueryUtils;
-import org.springframework.data.jpa.repository.support.Querydsl;
 import org.springframework.stereotype.Service;
-import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.example.filteringquerydsl.model.QGame.game;
-import static com.example.filteringquerydsl.model.QGamePlayer.gamePlayer;
-import static com.example.filteringquerydsl.model.QPlayer.player;
 
 @Service
 @RequiredArgsConstructor
 public class GameService {
-    
+
     private final JPAQueryFactory jpaQueryFactory;
     private final GameMapper gameMapper;
-    
-    public Page<GameFilterResponse> filterGames (List<String> opponentUsernames, LocalDateTime dateFrom,
-                                                 LocalDateTime dateTo,
-                                                 String gameStatus,
-                                                 String winnerUsername, Pageable pageable) {
+
+    /**
+     * Filters and paginates games based on various criteria.
+     *
+     * @param opponentUsernames List of opponent usernames to filter by.
+     * @param dateFrom          The starting game date range for filtering.
+     * @param dateTo            The ending game date range for filtering.
+     * @param gameStatus        The game status to filter by.
+     * @param winnerUsernames    List of winner usernames to filter by.
+     * @param pageable          Pageable object containing pagination and sorting information.
+     * @return A Page of {@link GameFilterResponse} containing the filtered and paginated results.
+     */
+    public Page<GameFilterResponse> filterGames(List<String> opponentUsernames, LocalDateTime dateFrom,
+                                                LocalDateTime dateTo,
+                                                String gameStatus,
+                                                List<String> winnerUsernames, Pageable pageable) {
         JPAQuery<Game> query = jpaQueryFactory.selectFrom(game);
-        
+
         // Apply filters
-        query = applyFilters(query, dateFrom, dateTo, opponentUsernames, gameStatus, winnerUsername);
-        
-        long totalElements = query.distinct().orderBy(getSortedColumn(pageable.getSort())).fetchCount();
-        
-        // Apply pagination
+        query = applyFilters(query, dateFrom, dateTo, opponentUsernames, gameStatus, winnerUsernames);
+
+
+        Sort sort = pageable.getSort();
+        OrderSpecifier<?>[] orderSpecifiers = getSortedColumn(sort);
+        // Calculate the total number of elements
+        long totalElements = query.distinct().fetchCount();
+
+        //Apply pagination and sorting
+        query.orderBy(orderSpecifiers);
         query.offset(pageable.getOffset()).limit(pageable.getPageSize());
-        
+
+        // Fetch the filtered games
         List<Game> filteredGames = query.fetch();
+
+
         List<GameFilterResponse> gameFilterResponses = filteredGames.stream()
-                                                                    .map(gameMapper::convertGameToGameResponse)
-                                                                    .collect(Collectors.toList());
-        
+                .map(gameMapper::convertGameToGameResponse)
+                .collect(Collectors.toList());
+
         return new PageImpl<>(gameFilterResponses, pageable, totalElements);
     }
-    private OrderSpecifier<?>[] getSortedColumn(Sort sorts){
-        return sorts.toList().stream().map(x ->{
-            Order order = x.getDirection().name() == "ASC"? Order.ASC : Order.DESC;
+
+    /**
+     * Converts a list of Sort objects to an array of OrderSpecifier objects for sorting.
+     *
+     * @param sorts The list of Sort objects representing sorting criteria.
+     * @return An array of OrderSpecifier objects for sorting.
+     */
+    private OrderSpecifier<?>[] getSortedColumn(Sort sorts) {
+        return sorts.toList().stream().map(x -> {
+            Order order = x.getDirection().name() == "ASC" ? Order.ASC : Order.DESC;
             SimplePath<Game> filedPath = Expressions.path(Game.class, game, x.getProperty());
             return new OrderSpecifier(order, filedPath);
         }).toArray(OrderSpecifier[]::new);
     }
-    /*private OrderSpecifier<?> parseSort(Sort.Order order) {
-        if (order.isAscending()) {
-            return Expressions.path(Object.class, game, order.getProperty()).asc();
-        } else {
-            return Expressions.path(Object.class, game, order.getProperty()).desc();
-        }
-    }*/
-    
-    
-    private JPAQuery<Game> applyFilters (JPAQuery<Game> query, LocalDateTime dateFrom, LocalDateTime dateTo,
-                                         List<String> opponentUsernames, String gameStatus, String winnerUsername) {
+
+    /**
+     * Applies filters to the JPAQuery based on various criteria.
+     *
+     * @param query             The JPAQuery instance to apply the filters to.
+     * @param dateFrom          The starting date range for filtering.
+     * @param dateTo            The ending date range for filtering.
+     * @param opponentUsernames List of opponent usernames to filter by.
+     * @param gameStatus        The game status to filter by.
+     * @param winnerUsernames    The winner's username to filter by.
+     * @return The modified JPAQuery with filters applied.
+     */
+    private JPAQuery<Game> applyFilters(JPAQuery<Game> query, LocalDateTime dateFrom, LocalDateTime dateTo,
+                                        List<String> opponentUsernames, String gameStatus, List<String> winnerUsernames) {
         query = filterByDate(query, dateFrom, dateTo);
         query =
-                filterByOpponentUsernames(query, opponentUsernames)/*.offset(pageable.getOffset()).limit(pageable
-                .getPageSize())*/;
+                filterByOpponentUsernames(query, opponentUsernames);
         query = filterByStatus(query, gameStatus);
-        query = filterByWinners(query, winnerUsername);
+        query = filterByWinners(query, winnerUsernames);
         return query;
     }
-    
-    private JPAQuery<Game> filterByDate (JPAQuery<Game> query, LocalDateTime dateFrom, LocalDateTime dateTo) {
+
+    /**
+     * Applies filtering based on the date range.
+     *
+     * @param query    The JPAQuery instance to apply the filter to.
+     * @param dateFrom The starting game date range for filtering.
+     * @param dateTo   The ending game date range for filtering.
+     * @return The modified JPAQuery with the date filter applied.
+     */
+    private JPAQuery<Game> filterByDate(JPAQuery<Game> query, LocalDateTime dateFrom, LocalDateTime dateTo) {
         if (dateFrom != null) {
             query = query.where(game.date.after(dateFrom)
-                                         .or(game.date.eq(dateFrom)));
+                    .or(game.date.eq(dateFrom)));
         }
         if (dateTo != null) {
             query = query.where(game.date.before(dateTo)
-                                         .or(game.date.eq(dateTo)));
+                    .or(game.date.eq(dateTo)));
         }
         return query;
     }
-    
-    private JPAQuery<Game> filterByOpponentUsernames (JPAQuery<Game> query, List<String> usernames) {
-        if (usernames != null && ! usernames.isEmpty()) {
-            
+
+    /**
+     * Applies filtering based on opponent usernames.
+     *
+     * @param query     The JPAQuery instance to apply the filter to.
+     * @param usernames The list of opponent usernames to filter by.
+     * @return The modified JPAQuery with the opponent username filter applied.
+     */
+    private JPAQuery<Game> filterByOpponentUsernames(JPAQuery<Game> query, List<String> usernames) {
+        if (usernames != null && !usernames.isEmpty()) {
+
             // Mora da se doda ovaj deo jer se u suprotnom javalja greska AliasCollisionException ako se filtriraju
             // npr oppUsernames i winnerUsername u isto vreme.
             QGamePlayer opponentGamePlayer = new QGamePlayer("opponentGamePlayer");
@@ -112,28 +148,43 @@ public class GameService {
         }
         return query;
     }
-    
-    
-    private JPAQuery<Game> filterByStatus (JPAQuery<Game> query, String gameStatus) {
+
+    /**
+     * Applies filtering based on game status.
+     *
+     * @param query      The JPAQuery instance to apply the filter to.
+     * @param gameStatus The game status to filter by.
+     * @return The modified JPAQuery with the game status filter applied.
+     */
+    private JPAQuery<Game> filterByStatus(JPAQuery<Game> query, String gameStatus) {
         if (gameStatus != null) {
             query = query.where(game.status.eq(Game.Status.valueOf(gameStatus)));
         }
         return query;
     }
-    
-    private JPAQuery<Game> filterByWinners (JPAQuery<Game> query, String winnerUsername) {
-        
-        
-        if (winnerUsername != null && ! winnerUsername.isEmpty()) {
+
+    /**
+     * Applies filtering based on game winners' usernames.
+     *
+     * @param query          The JPAQuery instance to apply the filter to.
+     * @param winnerUsernames List of winner usernames to filter by.
+     * @return The modified JPAQuery with the winner username filter applied.
+     */
+    private JPAQuery<Game> filterByWinners(JPAQuery<Game> query, List<String> winnerUsernames) {
+
+
+        if (winnerUsernames != null && !winnerUsernames.isEmpty()) {
+            // Mora da se doda ovaj deo jer se u suprotnom javalja greska AliasCollisionException ako se filtriraju
+            // npr oppUsernames i winnerUsername u isto vreme.
             QGamePlayer winnerGamePlayer = new QGamePlayer("winnerGamePlayer");
             QPlayer winnerPlayer = new QPlayer("winnerPlayer");
-            
+
             query = query.leftJoin(game.gamePlayers, winnerGamePlayer)
-                         .leftJoin(winnerGamePlayer.player, winnerPlayer)
-                         .where(winnerGamePlayer.isWinner.eq(true)
-                                                   .and(winnerPlayer.username.eq(winnerUsername)));
+                    .leftJoin(winnerGamePlayer.player, winnerPlayer)
+                    .where(winnerGamePlayer.isWinner.eq(true)
+                            .and(winnerPlayer.username.in(winnerUsernames)));
         }
         return query;
     }
-    
+
 }
